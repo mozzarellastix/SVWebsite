842c568c272868b3b8b89117c563fea5adf38365
mozzarellastix <silviaosuna1104@gmail.com>
2024-03-14 12:52:19 -0500

    Changed some css and implemented new radio buttons for spall.

--- form.js
+++ form.js
@@ -4,14 +4,13 @@ dataForm.addEventListener("submit", (e) => {
     //Keeps the page from refreshing on submit.
     e.preventDefault();
     
-    let rpm;
-    let temperatureC; 
-    let temperatureF;
-    let cup;
-    let cone; 
+    let rpm, temperatureC, temperatureF, cup, cone;
 
     let loaded = document.getElementById('loaded').checked;
     let unloaded = document.getElementById('unloaded').checked;
+
+    let cupChecked = document.getElementById('cup').checked;
+    let coneChecked = document.getElementById('cone').checked;
 
     let speedElement = document.getElementById("speed");
     let temperatureElementC = document.getElementById("tempResult");
@@ -24,14 +23,11 @@ dataForm.addEventListener("submit", (e) => {
     let cupElement = document.getElementById("cupResult");
 
     //reset strings for next submit call
-    speedElement.innerText = "";
-    temperatureElementC.innerText = "";
-    temperatureElementF.innerText = "";
-    coneElement.innerText = "";
-    cupElement.innerText = "";
 
-    console.log("loaded:" + loaded);
-    console.log("unloaded: " + unloaded);
+    speedElement.innerText = temperatureElementC.innerText = temperatureElementF.innerText = coneElement.innerText = cupElement.innerText = "";
+
+    console.log("cone checked: " + coneChecked);
+    console.log("cup checked: " + cupChecked);
     
     //Grab our values for speed from the html file
     //This will either be in mph or kmph depending on what the user selects
@@ -42,86 +38,86 @@ dataForm.addEventListener("submit", (e) => {
 
     let convertEquation = 60 * 2 *(Math.PI) * 18 * 0.0254;
 
-    if (selectedUnit == "mph"){
+    if (selectedUnit == "mph")
         rpm = (speedInput.value)/(convertEquation * (1/1609));
-    }
+    
 
-    else if (selectedUnit == "kmph"){
+    else if (selectedUnit == "kmph")
         rpm = (speedInput.value * 1000)/(convertEquation)
-    }
 
     //if they chose unloaded and then healthy, calculate the control temp 
-    
-    //Display control temperatures if they select healthy
-    console.log("rpm: " + rpm);
 
     if (document.getElementById("healthy").checked){
         //if loaded was selected, calculate temperature 100
-        if (loaded){
+        if (loaded)
             temperatureC = (0.0704 * rpm) + 5.355;
-        }
-        else{
+        else
             temperatureC = (0.0622 * rpm) - 3.618;
-        }
+
         temperatureF = (temperatureC * (9/5)) + 32;
         
+        console.log("unrounded value for celcius: " + temperatureC);
+        console.log("unrounded value for fahrenheit: " + temperatureF);
+        
+        temperatureC = Number(temperatureC.toFixed(2))
+        temperatureF = Number(temperatureF.toFixed(2))
+
         temperatureElementC.innerText = temperatureC;
         temperatureElementF.innerText = temperatureF;
-
     }
 
     else if (document.getElementById("defective").checked){
         if (loaded){
-            cup = (0.0659 * rpm) + 8.1054;
-            cone = (0.0859 * rpm) + 2.1146;
-            temperatureC = (0.0704 * rpm) + 5.355;
+            if (cupChecked){
+                cup = (0.0659 * rpm) + 8.1054;
+                cup = Number(cup.toFixed(2));
+                cupElement.innerText = cup;
+            }
+
+            else if (coneChecked){
+                cone = (0.0859 * rpm) + 2.1146;
+                cone = Number(cone.toFixed(2));
+                coneElement.innerText = cone;
+            }
+           temperatureC = (0.0704 * rpm) + 5.355;
         }
+
         else if (unloaded){
-            cup = (0.0615 * rpm) - 3.8473;
-            cone = (0.072 * rpm) - 2.994;
+            if (cupChecked){
+                cup = (0.0615 * rpm) - 3.8473;
+                cup = Number(cup.toFixed(2));
+                cupElement.innerText = cup;
+            }
+            else if (coneChecked){
+                cone = (0.072 * rpm) - 2.994;
+                cone = Number(cone.toFixed(2));
+                coneElement.innerText = cone;
+            }
             temperatureC = (0.0622 * rpm) - 3.618;
         }
         temperatureF = (temperatureC * (9/5)) + 32;
-        console.log("tempc:" + temperatureC);
-        console.log("tempf:" + temperatureF);
+
+        temperatureC = Number(temperatureC.toFixed(2));
+        temperatureF = Number(temperatureF.toFixed(2));
+
         temperatureElementC2.innerText = temperatureC;
         temperatureElementF2.innerText = temperatureF;
-        coneElement.innerText = cone;
-        cupElement.innerText = cup;
+
+        // coneElement.innerText = cone;
+        // cupElement.innerText = cup;
     }
 
     //Reset the values for the next submit
-    rpm = 0;
-    temperatureC = 0;
-    temperatureF = 0;
-    cone = 0;
-    cup = 0;
+
+    rpm = temperatureC = temperatureF = cone = cup = 390;
 
     speedElement.innerText = rpm;
-
-    //check which dropdown was selected. 
-    //if mph, convert the mph to rpm
-    //if kmph, convert the kmph to rpm
-
-    // //Grab our other values from the html page.
-    // let unloadedTemp = document.getElementById("unloadedResult");
-    // let loadedTemp = document.getElementById("loaded");
 
     // let maxThresholdResult = document.getElementById("maxThresholdResult");
     // let prelimThresholdResult = document.getElementById("preThresholdResult");
 
     // let preliminaryThreshold = (5.779 * (0.001 * rpm.value) - 0.1202); //5.779 * 10^-3(rpm) - 1.202 * 10^-1
     // let maxThreshold = (1.13 * (0.01 * rpm.value) - 0.839); //1.13*10^-2(rpm)-0.839
-
-
-    // // if (rpm.value == "" || vibration.value == ""){
-    // //     console.error("One of the values is empty.");
-    // // }
-    // // else{
-    // //     console.log("Value in vibration:" + vibration.value);
-    // //     console.log("Value in rpm:" + rpm.value);
-    // //     console.log("temperature100: " + temperature100);
-    // // }
     
     // rmsInput.innerText = rpm.value;
     // rpm.value = ""; //reset the rpm for the next input
--- functions.js
+++ functions.js
@@ -2,22 +2,30 @@ function check(){
     if (document.getElementById("defective").checked ){
         document.getElementById("spallInput").style.display = "block";
     }
-    else{
+    else if (document.getElementById("healthy").checked){
         document.getElementById("spallInput").style.display = "none";
     }
 }
 
 function unhide(){
     if(document.getElementById("healthy").checked){
+        // Display the healthy results while defective results stay hidden
         document.getElementById("healthyResults").style.display="block";
         document.getElementById("defectiveResults").style.display="none";
     }
     
     else if(document.getElementById("defective").checked){
+        // Display the defective results while healthy results stay hidden
         document.getElementById("defectiveResults").style.display="block";
         document.getElementById("healthyResults").style.display="none";
+
+        if (document.getElementById('cup').checked){
+            document.getElementById("cupResults").style.display="block";
+            document.getElementById("coneResults").style.display="none";
+        }
+        else if(document.getElementById('cone').checked){
+            document.getElementById("cupResults").style.display="none";
+            document.getElementById("coneResults").style.display="block";
+        }
     }
-    //check if the user chose healthy or defective
-    //1st option: chose healthy...display temperature
-    //2nd option: chose defective... display cup and cone 
 }
